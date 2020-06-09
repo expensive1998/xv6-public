fe8a6eccebaf32b15ed66ffeced0b21960055e93
expensive1998 <aligranmaye98@gmail.com>
2020-06-09 01:59:30 -0700

    handle test file

--- test.c
+++ test.c
@@ -7,3 +7,41 @@ struct proc_info
   int pid;
   int memsize;
 }; 
+
+int
+main(void)
+{
+    if (fork() != 0)
+    {
+        if (fork())
+        {
+            malloc(40));
+        }
+        else
+        {
+            malloc(20);
+        }
+        wait();
+    }
+    else
+    {
+        if (fork())
+        {
+            malloc(30);
+        }
+        else
+        {
+            malloc(50);
+        }
+        wait();
+    }
+    int max_p = 20;
+    struct proc_info *allProcess = malloc(max_p*sizeof(struct proc_info));
+    int N = procs(max_p, allProcess);
+    for (int i = 0; i < N; i++)
+    {
+        printf(1, "pid : %d  memory size : %d \n", allProcess[i].pid, allProcess[i].memsize);
+    }
+    wait();
+    exit();
+}
